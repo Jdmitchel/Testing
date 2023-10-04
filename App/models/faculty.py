4e4485d03334e81be7246c70efa95f84f920a23e
Jardel - JD <mitchelljardel@gmail.com>
2023-10-03 20:14:51 -0400

    db update minor

--- App/models/faculty.py
+++ App/models/faculty.py
@@ -6,7 +6,7 @@ class Faculty(db.Model):
     facultyName = db.Column(db.String, nullable=False, unique=True)
 
     students = db.relationship('Student', backref='faculty', lazy=True)
-    courses = db.relationship('Courses', backref='faculty', lazy=True)
+    courses = db.relationship('Course', backref='faculty', lazy=True)
 
     def __init__(self, facultyName):
         self.facultyName = facultyName
